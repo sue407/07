4ac7cfe467f17cd4e6063e20c19bc309fbc9a4f7
sue407 <sue407407@gmail.com>
2022-10-18 15:52:50 +0900

    07 (2)
    10/18 (2)

--- main.c
+++ main.c
@@ -4,15 +4,13 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(void) {
-	int y;
-	int flag = 1;
-	while(flag != 0)
+	int i;
+	for (i=0;i<5;i++)
 	{
-		int y;
-		y = 3;
-		flag = 0;
+	  int temp = 1;
+	  printf("temp = %d\n", temp);
+	  temp++;
 	}
-	y=4;
-	
 	return 0;
+	
 }
